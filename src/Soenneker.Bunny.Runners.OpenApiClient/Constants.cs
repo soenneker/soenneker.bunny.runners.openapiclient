79ea9794416779149e397e52fb90da6f89721149
Jake Soenneker <jake@soenneker.com>
2026-06-01 16:09:25 -0500

    Initial

---
+++ src/Soenneker.Bunny.Runners.OpenApiClient/Constants.cs
@@ -0,0 +1,6 @@
+namespace Soenneker.Bunny.Runners.OpenApiClient;
+
+public static class Constants
+{
+    public const string Library = "Soenneker.Bunny.OpenApiClient";
+}
---
+++ test/Soenneker.Bunny.Runners.OpenApiClient.Tests/BunnyOpenApiClientRunnerTests.cs
@@ -0,0 +1,18 @@
+using Soenneker.Tests.HostedUnit;
+
+namespace Soenneker.Bunny.Runners.OpenApiClient.Tests;
+
+[ClassDataSource<Host>(Shared = SharedType.PerTestSession)]
+public sealed class BunnyOpenApiClientRunnerTests : HostedUnitTest
+{
+    public BunnyOpenApiClientRunnerTests(Host host) : base(host)
+    {
+    }
+
+    [Test]
+    [Skip("Manual")]
+    public void Default()
+    {
+
+    }
+}
